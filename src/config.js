086ef72d828cd719570864ba96f67714cc595325
Madasar-Farooq <mudasarhassan516@gmail.com>
2025-05-12 18:52:46 +0500

    supabase key removed

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://vtyuwxeyousmiihfulaf.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0eXV3eGV5b3VzbWlpaGZ1bGFmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NTI1NDAsImV4cCI6MjA2MjAyODU0MH0.srP-8ZF8O3izYkFVlrja2PqKe6SS6sa3maefZxSzPqs"
+const supabaseKey = ""
 export const supabase = createClient(supabaseUrl, supabaseKey);
