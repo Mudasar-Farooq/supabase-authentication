a767bbe44f22727ca1a8ca77f206debe8e2ccfb7
Madasar-Farooq <mudasarhassan516@gmail.com>
2025-05-12 20:39:38 +0500

    again commit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://vtyuwxeyousmiihfulaf.supabase.co'
-const supabaseKey = ""
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+console.log(supabaseUrl)
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
